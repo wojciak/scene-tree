7f48f1a5de5478ca3522f00fc23ef8ecbdc62a08
Łukasz Wójciak <kontakt@wojciak.com.pl>
2016-09-12 09:45:48 +0200

    State the role of this thing

--- app/app.ts
+++ app/app.ts
@@ -7,3 +7,8 @@ class App {
 let app = new App();
 
 export {app}
+
+/**
+ * Scene graph (parent/child trees), each state node can be the root of a new leaf
+ * FSM - each scene can have a defined set of state transitions(actions), the actions propagate down the tree
+ */
